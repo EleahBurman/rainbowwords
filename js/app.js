652ec70b6e1063f9bb4f65299c65763ee5b5aa1f
EleahBurman <eleahjburman@gmail.com>
2023-03-31 15:26:29 -0400

    Lists const winning words

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-//include variables with the term let for board, turnsleft, winner, message (winning and losing)
+//include variables with the term let for board (build logic to set that board from the imported boards), turnsleft, winner, message (winning and losing)
 /*------------------------ Cached Element References ------------------------*/
 // include losingSquares (random letters), using query selector, loop, math floor and math random
 // inclue message every time someone chooses a wrong word using query selector
@@ -17,7 +17,7 @@
 // include winning message telling them if they get gold, silver or bronze trophies
 /*----------------------------- Event Listeners -----------------------------*/
 //squareChoices targeting the item click square to invoke handle click flunction
-//listens for the click of the reset button and effects function turn
+//listens for the click of the reset button and effects function turn, and initializer, new game
 //lightvDark button to click for a light rainbow mode versus dark rainbow mode
 /*-------------------------------- Functions --------------------------------*/
 //initializer for the board, turnsLeft, winner, render
@@ -26,8 +26,8 @@
 //function renderboard - uses forEach as it iterates over the squares through the board
 //function updateMessage using if winning option, else if losing option
 //function winningMessage if and else if for gold (6 guesses), silver(7-8 guesses), and bronze(9 to 10 guesses)  using innerHTML, also include playBornThisWay (will include clip in a separate audio.js)
-//function handleClick using parseInt to make each space into numbers for each board - board one is A1-100, board 2 is B1-100 for each space. There will be 6 boards. Include checkWinWord and render
+//function handleClick using parseInt to make each space into numbers for each board - board one is A1-100, board 2 is B1-100 for each space. There will be 6 boards. Include checkWinword and render
 //function turn, using for loop that needs to be less than 10 turns. Resets with reset button
-//function checkWinningoption using for each, mathabs, and the const randomWinningwords, and set to winner=true
+//function checkWinword using for each, mathabs, and the const randomWinningwords, and set to winner=true
 //function toggleLightRainbow using classes can toggle from a lighter rainbow scheme to a darker rainbow scheme
 //function checkDark will check if someone prefers dark mode over light mode using window match media and class name
