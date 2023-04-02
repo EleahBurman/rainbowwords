836a9a21f18519db8fb9546cb4433a6f87ccb1e0
EleahBurman <eleahjburman@gmail.com>
2023-04-02 18:54:00 -0400

    Random letters set to losing spaces

--- js/app.js
+++ js/app.js
@@ -4,14 +4,18 @@
 
 /*-------------------------------- Constants --------------------------------*/
 //audio from audio.js
-//winning squares given for each board with the string letters attached to each spot - in total there will be 6 words correct on each of the 6 boards - total of 36 words -- should I get rid of letters and make just numbers - would that make easier for later?
+//create objects within an array that is equal to winningwords
 const winningWordsa = [
-  {A0: 'N', A1: 'O', A2: 'N', A3: 'B', A4: 'I', A5: 'N', A6: 'A', A7: 'R', A8: 'Y'},
-  {D2: 'G', D3: 'E', D4: 'N', D5: 'D', D6: 'E', D7: 'R'},
-  {F0: 'G', F1: 'A', F2: 'Y'},
-  {G3: 'T', G4: 'R', G5: 'A', G6: 'N', G7: 'S'},
-  {H3: 'F', H4: 'E', H5: 'M', H6: 'M', H7: 'E'},
-  {B9: 'L', C9: 'E', D9: 'S', E9: 'B', F9: 'I', G9: 'A', H9: 'N'}
+  {A0: 'n', A1: 'o', A2: 'n', A3: 'b', A4: 'i', A5: 'n', A6: 'a', A7: 'r', A8: 'y'},
+  {D2: 'g', D3: 'e', D4: 'n', D5: 'd', D6: 'e', D7: 'r'},
+  {F0: 'g', F1: 'a', F2: 'y'},
+  {G3: 't', G4: 'r', G5: 'a', G6: 'n', G7: 's'},
+  {H3: 'f', H4: 'e', H5: 'm', H6: 'm', H7: 'e'},
+  {B9: 'l', C9: 'E', D9: 's', E9: 'b', F9: 'i', G9: 'a', H9: 'n'}
+]
+//create a const for random letters assign a random letter to open strings that are not set by winning words
+const randomLetters = [
+  {A9: '',B0: '', B1: '', B2: '', B3: '', B4: '', B5: '', B6: '', B7: '', B8: '', C0: '', C1: '', C2: '', C3: '', C4: '', C5: '', C6: '', C7: '', C8: '', D0: '', D1: '', D8: '', E0: '', E1: '', E2: '', E3: '', E4: '', E5: '', E6: '', E7: '', E8: '', F3: '', F4: '', F5: '', F6: '', F7: '', F8: '', G0: '', G1: '', G2: '', G8: '', H0: '', H1: '', H2: '', I0: '', I1: '', I2: '', I3: '', I4: '', I5: '', I6: '', I7: '', I8: '', I9: '', J0: '', J1: '', J2: '', J3: '', J4: '', J5: '', J6: '', J7: '', J8: '', J9: ''}
 ]
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -31,19 +35,39 @@ const resetBtn = document.querySelector('#reset-buton')
 /*-------------------------------- Functions --------------------------------*/
 //go through each element in the winning words array and each key becomes an id in html (find elements by using ids)
 function setBoard (){
+  //for each object we iterate through the winning words array and look at each object
   winningWordsa.forEach(obj=> {
-    //for each object we iterate through the winning words array and look at each object
+    //we used object.keys to get keys included in an object as strings
     Object.keys(obj).forEach(key=>{
       console.log(key)
-      //we used object.keys to get keys included in an object as strings
       const winningLetter = document.querySelector(`#${key}`)
       console.log(winningLetter)
       //dynamically pass a key through an object to access a value by using bracket notation
       winningLetter.textContent = obj[key]
     })
-  });
+  })
 }
 setBoard()
+
+function getRandomletter(x){
+    //alphabet is a string of every letter
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+        'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u',
+        'v', 'w', 'x', 'y', 'z']
+    let emptyString = ''
+    randomLetters.forEach(obj2=> {
+      Object.keys(obj2).forEach(key2=>{
+      //returns values
+        const losingLetter = document.querySelector(`#${key2}`) 
+        //key 2 is now changed from an empty string to a random letter of alphabet
+        key2 = (emptyString + `${alphabet[(Math.floor(Math.random() * 26))]}`)
+        losingLetter.textContent = key2
+        console.log(key2)
+      })
+    })
+}
+getRandomletter()
 
 //initializer for the board, turnsLeft, winner, render
 //function render that has updateBoard, renderBoard, updateMessage, winningMessage within it
