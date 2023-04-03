6c1fe1a6446f2953218cde7cc43d8c1ef9a60900
EleahBurman <eleahjburman@gmail.com>
2023-04-03 16:07:08 -0400

    Fixed bug in checkwinword

--- js/app.js
+++ js/app.js
@@ -15,9 +15,6 @@ const winningWordsa = [
   {B9: 'l', C9: 'e', D9: 's', E9: 'b', F9: 'i', G9: 'a', H9: 'n'},
 ]
 
-//hardcoded location which is not correct
-const locationWinningvalues = ['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A8', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'F0', 'F1', 'F2', 'G3', 'G4', 'G5', 'G6', 'G7', 'H3', 'H4', 'H5', 'H6', 'H7', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9'
-]
 //create a const for random letters assign a random letter to open strings that are not set by winning words
 const randomLetters = [
   {A9: '',B0: '', B1: '', B2: '', B3: '', B4: '', B5: '', B6: '', B7: '', B8: '', C0: '', C1: '', C2: '', C3: '', C4: '', C5: '', C6: '', C7: '', C8: '', D0: '', D1: '', D8: '', E0: '', E1: '', E2: '', E3: '', E4: '', E5: '', E6: '', E7: '', E8: '', F3: '', F4: '', F5: '', F6: '', F7: '', F8: '', G0: '', G1: '', G2: '', G8: '', H0: '', H1: '', H2: '', H8: '', I0: '', I1: '', I2: '', I3: '', I4: '', I5: '', I6: '', I7: '', I8: '', I9: '', J0: '', J1: '', J2: '', J3: '', J4: '', J5: '', J6: '', J7: '', J8: '', J9: ''}
@@ -26,7 +23,7 @@ const randomLetters = [
 
 /*---------------------------- Variables (state) ----------------------------*/
 //include variables with the term let for board (build logic), turnsleft, winner, message (winning and losing) (why is board One not working)
-let winner, turn
+let winner, turn, boardVal
 /*------------------------ Cached Element References ------------------------*/
 // square choices are equal to each div in class column
 const squareChoices = document.querySelectorAll('.column')
@@ -58,6 +55,7 @@ function init (){
     null, null, null, null, null, null, null, null, null, null,
     null, null, null, null, null, null, null, null, null, null,
     null, null, null, null, null, null, null, null, null, null,]
+  boardVal = 0
     message.textContent = 'Click each 🌈 letter'
   //updateBoard()
   removeRainbow()
@@ -86,22 +84,6 @@ function setBoard (){
 console.log(setBoard, 'winning words set')
 setBoard()
 
-//updateBoard will change the null spot to 1 if the letter is in the board
-// function updateBoard(boardVal, idx){
-//   if(boardVal === null && (squareChoices.inword){
-//     (boardVal + 1)
-//     changeTurn()
-//     rainbow()
-//   }
-//   else if(boardVal && squareChoices.inword){
-//     (boardVal + 1)
-//     changeTurn()
-//     rainbow()
-//   }
-//   else if(!squareChoices.inword){
-//   changeTurn()
-//   }
-// }
 function getRandomletter(x){
   //alphabet is a string of every letter
     let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
@@ -124,18 +106,33 @@ getRandomletter()
 
 function handleClick(event) {
   const sqrIdx = event.target.id
-  console.log('sqrIdx',sqrIdx)
 
   winningWordsa.forEach((word) => {
     if (word[sqrIdx]) {
-      console.log('is this on?')
       rainbow(event.target.id)
+      updateBoard()
       //checkBox()
     }
   })
 }
-//function checkBox adds 1 every time a turn goes down by one && in word is true
-//function checkWinword uses the total of each word is equivalent to the values from board
+
+//updateBoard will change the null spot to add 1 if the letter is in the board
+function updateBoard(boardVal, idx){
+  boardVal ++
+}
+
+function checkWinword(){
+  //if current boardval = the amount of correct square choices
+  let total = winningWordsa.reduce((acc, word) => {
+    //we are adding the length of each object in the array to get a total value
+    return acc + Object.keys(word).length
+    console.log(acc)
+    //starting value is 0
+  }, 0)
+  if (boardVal === total){
+    winner
+  }
+}
 
 function rainbow (id) {
   //created element which is equivalent to the the div you click on 
